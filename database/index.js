beeac2f867d9f706f0ab14497e5a1317604044a1
Carmen Hilbert <carminaburana512@gmail.com>
2021-03-18 15:16:38 -0400

    reviews csv imported

--- database/index.js
+++ database/index.js
@@ -5,6 +5,23 @@ const sql = postgres
 
 //sudo -u postgres -i
 
-//CREATE TABLE IF NOT EXISTS Public."reviews"(Id int, product_id int, date date, summary varchar(1000), body varchar(1000), reported bool, reviewer_name varchar(100), reviewer_email varchar(100), response varchar(1000), helpfulness int);
+DROP DATABASE IF EXISTS reviews;
+CREATE DATABASE reviews;
 
- \COPY Public."reviews" FROM '/Users/carmenhilbert/Downloads/reviews.csv' DELIMITER ',' CSV HEADER;
+
+CREATE TABLE IF NOT EXISTS Public."reviews"(
+  Id int,
+  product_id int,
+  rating int,
+  date date,
+  summary varchar(1000),
+  body varchar(1000),
+  recommend bool,
+  reported bool,
+  reviewer_name varchar(100),
+  reviewer_email varchar(100),
+  response varchar(1000),
+  helpfulness int
+  );
+
+ \COPY Public."reviews" FROM '/Users/carmenhilbert/Public/catwalk_data/reviews.csv' DELIMITER ',' CSV HEADER;
