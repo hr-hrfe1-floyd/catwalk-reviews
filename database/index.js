92590dcdfb7abb4a37dc25539860502da7fb38b3
Carmen Hilbert <carminaburana512@gmail.com>
2021-03-18 15:35:13 -0400

    all data imported into database

--- database/index.js
+++ database/index.js
@@ -24,4 +24,30 @@ CREATE TABLE IF NOT EXISTS Public."reviews"(
   helpfulness int
   );
 
+
+  CREATE TABLE IF NOT EXISTS Public."reviews_photos"(
+    Id int,
+    review_id int,
+    url varchar(1000)
+    );
+
+CREATE TABLE IF NOT EXISTS Public."characteristics"(
+  Id int,
+  product_id int,
+  name varchar(100)
+  );
+
+CREATE TABLE IF NOT EXISTS Public."characteristic_reviews"(
+  Id int,
+  characteristic_id int,
+  review_id int,
+  value int
+  );
+
  \COPY Public."reviews" FROM '/Users/carmenhilbert/Public/catwalk_data/reviews.csv' DELIMITER ',' CSV HEADER;
+
+ \COPY Public."reviews_photos" FROM '/Users/carmenhilbert/Public/catwalk_data/reviews_photos.csv' DELIMITER ',' CSV HEADER;
+
+ \COPY Public."characteristics" FROM '/Users/carmenhilbert/Public/catwalk_data/characteristics.csv' DELIMITER ',' CSV HEADER;
+
+ \COPY Public."characteristic_reviews" FROM '/Users/carmenhilbert/Public/catwalk_data/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
